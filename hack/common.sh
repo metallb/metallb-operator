31c623fbcc8d59710744f0ccec9e2dcc6fa0649e
Ori Braunshtein <obraunsh@redhat.com>
2021-06-16 09:44:12 +0300

    Use the MetalLB git repo as the source of truth for configuration
    Add scripts to generate and verify MetalLB manifests using the main repo
    
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

---
+++ hack/common.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+pushd .
+cd "$(dirname "$0")/.."
+
+function finish {
+    popd
+}
+trap finish EXIT
+
+GOPATH="${GOPATH:-~/go}"
+export GOFLAGS="${GOFLAGS:-"-mod=vendor"}"
+
+export PATH=$PATH:$GOPATH/bin
+
+mkdir -p _cache
