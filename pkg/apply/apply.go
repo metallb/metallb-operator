42643470e2847f5303b744aada2bb529587a12f2
Mohamed Mahmoud <mmahmoud@redhat.com>
2021-07-06 21:07:50 -0400

    fix merge conflicts
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- pkg/apply/apply.go
+++ pkg/apply/apply.go
@@ -14,13 +14,12 @@ import (
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// ApplyObject applies the desired object against the apiserver,
-// merging it with any existing objects if already present.
-func ApplyObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstructured) error {
+// Find existing object or create if if it doesn't exists
+func findOrCreateObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstructured) (*uns.Unstructured, string, error) {
 	name := obj.GetName()
 	namespace := obj.GetNamespace()
 	if name == "" {
-		return errors.Errorf("Object %s has no name", obj.GroupVersionKind().String())
+		return nil, "", errors.Errorf("Object %s has no name", obj.GroupVersionKind().String())
 	}
 	gvk := obj.GroupVersionKind()
 	// used for logging and errors
@@ -28,7 +27,7 @@ func ApplyObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstruct
 	log.Printf("reconciling %s", objDesc)
 
 	if err := IsObjectSupported(obj); err != nil {
-		return errors.Wrapf(err, "object %s unsupported", objDesc)
+		return nil, objDesc, errors.Wrapf(err, "object %s unsupported", objDesc)
 	}
 
 	// Get existing
@@ -40,11 +39,25 @@ func ApplyObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstruct
 		log.Printf("does not exist, creating %s", objDesc)
 		err := client.Create(ctx, obj)
 		if err != nil {
-			return errors.Wrapf(err, "could not create %s", objDesc)
+			return nil, objDesc, errors.Wrapf(err, "could not create %s", objDesc)
 		}
 		log.Printf("successfully created %s", objDesc)
+		return obj, objDesc, nil
+	}
+
+	return existing, objDesc, err
+}
+
+// ApplyObject applies the desired object against the apiserver,
+// merging it with any existing objects if already present.
+func ApplyObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstructured) error {
+
+	existing, objDesc, err := findOrCreateObject(ctx, client, obj)
+
+	if existing == nil {
 		return nil
 	}
+
 	if err != nil {
 		return errors.Wrapf(err, "could not retrieve existing %s", objDesc)
 	}
@@ -55,6 +68,42 @@ func ApplyObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstruct
 	}
 	if !equality.Semantic.DeepEqual(existing, obj) {
 		if err := client.Update(ctx, obj); err != nil {
+			return errors.Wrapf(err, "could not update object %s", objDesc)
+		} else {
+			log.Printf("update was successful")
+		}
+	}
+
+	return nil
+}
+
+// ApplyObjects it applies a list of desired objects after merging them.
+func ApplyObjects(ctx context.Context, client k8sclient.Client, objs []*uns.Unstructured) error {
+
+	var lastObj, existing *uns.Unstructured = nil, nil
+	var objDesc string = ""
+	var err error = nil
+
+	existing, objDesc, err = findOrCreateObject(ctx, client, objs[0])
+	if existing == nil {
+		return nil
+	}
+	if err != nil {
+		return errors.Wrapf(err, "could not retrieve existing %s", objDesc)
+	}
+
+	for _, obj := range objs {
+		if lastObj != nil {
+			// Merge the desired object with what actually exists
+			if err := MergeObjectForUpdate(existing, obj); err != nil {
+				return errors.Wrapf(err, "could not merge object %s with existing", objDesc)
+			}
+		}
+		lastObj = obj
+	}
+
+	if !equality.Semantic.DeepEqual(existing, lastObj) && lastObj != nil {
+		if err := client.Update(ctx, lastObj); err != nil {
 			return errors.Wrapf(err, "could not update object %s", objDesc)
 		} else {
 			log.Printf("update was successful")
