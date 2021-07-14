5f2d05552e2a024b85e4eb8049798766ec68303d
Ori Braunshtein <obraunsh@redhat.com>
2021-07-14 12:12:06 +0300

    Capitalize correctly throughout the project
    Capitalize according to these rules:
    * "MetalLB Operator" -- the Operator name in general text, uppercase LB.
    * `metallb-<whatever>` -- API objects use inline code and MetalLB is all lowercase
    * "MetalLB" -- the name of the load balancer implementation in general text, uppercase LB.
    
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

--- test/consts/const.go
+++ test/consts/const.go
@@ -1,22 +1,22 @@
 package consts
 
 const (
-	// MetallbNameSpace contains the name of the MetalLB operator  namespace
-	MetallbNameSpace = "metallb-system"
-	// MetallbOperatorDeploymentName contains the name of the MetalLB operator deployment
-	MetallbOperatorDeploymentName = "metallboperator-controller-manager"
-	// MetallbOperatorDeploymentLabel contains the label of the MetalLB operator deployment
-	MetallbOperatorDeploymentLabel = "controller-manager"
-	// MetallbOperatorCRDName contains the name of the MetalLB operator CRD
-	MetallbOperatorCRDName = "metallbs.metallb.io"
-	// MetallbCRFile contains the Metallb custom resource deployment
-	MetallbCRFile = "metallb.yaml"
-	// MetallbDeploymentName contains the name of the MetalLB deployment
-	MetallbDeploymentName = "controller"
-	// MetallbDaemonsetName contains the name of the MetalLB daemonset
-	MetallbDaemonsetName = "speaker"
-	// MetallbAddressPoolCRDName contains the name of MetallB AddressPool CRD
-	MetallbAddressPoolCRDName = "addresspools.metallb.io"
-	// MetallbConfigMapName contains created configmap
-	MetallbConfigMapName = "config"
+	// MetalLBNameSpace contains the name of the MetalLB Operator  namespace
+	MetalLBNameSpace = "metallb-system"
+	// MetalLBOperatorDeploymentName contains the name of the MetalLB Operator deployment
+	MetalLBOperatorDeploymentName = "metallboperator-controller-manager"
+	// MetalLBOperatorDeploymentLabel contains the label of the MetalLB Operator deployment
+	MetalLBOperatorDeploymentLabel = "controller-manager"
+	// MetalLBOperatorCRDName contains the name of the MetalLB Operator CRD
+	MetalLBOperatorCRDName = "metallbs.metallb.io"
+	// MetalLBCRFile contains the MetalLB custom resource deployment
+	MetalLBCRFile = "metallb.yaml"
+	// MetalLBDeploymentName contains the name of the MetalLB deployment
+	MetalLBDeploymentName = "controller"
+	// MetalLBDaemonsetName contains the name of the MetalLB daemonset
+	MetalLBDaemonsetName = "speaker"
+	// MetalLBAddressPoolCRDName contains the name of MetallB AddressPool CRD
+	MetalLBAddressPoolCRDName = "addresspools.metallb.io"
+	// MetalLBConfigMapName contains created configmap
+	MetalLBConfigMapName = "config"
 )
