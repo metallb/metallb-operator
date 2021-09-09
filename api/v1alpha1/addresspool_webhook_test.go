e032a63bfee63d509a5919e8582f7e50632163c5
Sabina Aledort <saledort@redhat.com>
2021-09-09 17:33:26 +0300

    Add AddressPool Webhook validation unit tests

---
+++ api/v1alpha1/addresspool_webhook_test.go
@@ -0,0 +1,177 @@
+package v1alpha1
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	MetalLBTestNameSpace = "metallb-test-namespace"
+)
+
+func TestValidateAddressPool(t *testing.T) {
+	autoAssign := false
+	addressPool := AddressPool{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-addresspool",
+			Namespace: MetalLBTestNameSpace,
+		},
+		Spec: AddressPoolSpec{
+			Protocol: "layer2",
+			Addresses: []string{
+				"1.1.1.1-1.1.1.100",
+			},
+			AutoAssign: &autoAssign,
+		},
+	}
+	addressPoolList := &AddressPoolList{}
+	addressPoolList.Items = append(addressPoolList.Items, addressPool)
+
+	tests := []struct {
+		desc             string
+		addressPool      *AddressPool
+		isNewAddressPool bool
+		expectedError    string
+	}{
+		{
+			desc: "Second AddressPool, already defined name",
+			addressPool: &AddressPool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-addresspool",
+					Namespace: MetalLBTestNameSpace,
+				},
+				Spec: AddressPoolSpec{
+					Protocol: "layer2",
+					Addresses: []string{
+						"1.1.1.101-1.1.1.200",
+					},
+					AutoAssign: &autoAssign,
+				},
+			},
+			isNewAddressPool: true,
+			expectedError:    "duplicate definition of pool",
+		},
+		{
+			desc: "Second AddressPool, overlapping addresses defined by address range",
+			addressPool: &AddressPool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-addresspool2",
+					Namespace: MetalLBTestNameSpace,
+				},
+				Spec: AddressPoolSpec{
+					Protocol: "layer2",
+					Addresses: []string{
+						"1.1.1.15-1.1.1.20",
+					},
+					AutoAssign: &autoAssign,
+				},
+			},
+			isNewAddressPool: true,
+			expectedError:    "overlaps with already defined CIDR",
+		},
+		{
+			desc: "Second AddressPool, overlapping addresses defined by network prefix",
+			addressPool: &AddressPool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-addresspool2",
+					Namespace: MetalLBTestNameSpace,
+				},
+				Spec: AddressPoolSpec{
+					Protocol: "layer2",
+					Addresses: []string{
+						"1.1.1.0/24",
+					},
+					AutoAssign: &autoAssign,
+				},
+			},
+			isNewAddressPool: true,
+			expectedError:    "overlaps with already defined CIDR",
+		},
+		{
+			desc: "Second AddressPool, invalid CIDR, single address provided while expecting a range",
+			addressPool: &AddressPool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-addresspool2",
+					Namespace: MetalLBTestNameSpace,
+				},
+				Spec: AddressPoolSpec{
+					Protocol: "layer2",
+					Addresses: []string{
+						"1.1.1.15",
+					},
+					AutoAssign: &autoAssign,
+				},
+			},
+			isNewAddressPool: true,
+			expectedError:    "invalid CIDR",
+		},
+		{
+			desc: "Second AddressPool, invalid CIDR, first address of the range is after the second",
+			addressPool: &AddressPool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-addresspool2",
+					Namespace: MetalLBTestNameSpace,
+				},
+				Spec: AddressPoolSpec{
+					Protocol: "layer2",
+					Addresses: []string{
+						"1.1.1.200-1.1.1.101",
+					},
+					AutoAssign: &autoAssign,
+				},
+			},
+			isNewAddressPool: true,
+			expectedError:    "invalid IP range",
+		},
+		{
+			desc: "Second AddressPool, invalid ipv6 CIDR, single address provided while expecting a range",
+			addressPool: &AddressPool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-addresspool2",
+					Namespace: MetalLBTestNameSpace,
+				},
+				Spec: AddressPoolSpec{
+					Protocol: "layer2",
+					Addresses: []string{
+						"2000::",
+					},
+					AutoAssign: &autoAssign,
+				},
+			},
+			isNewAddressPool: true,
+			expectedError:    "invalid CIDR",
+		},
+		{
+			desc: "Second AddressPool, invalid ipv6 CIDR, first address of the range is after the second",
+			addressPool: &AddressPool{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-addresspool2",
+					Namespace: MetalLBTestNameSpace,
+				},
+				Spec: AddressPoolSpec{
+					Protocol: "layer2",
+					Addresses: []string{
+						"2000:::ffff-2000::",
+					},
+					AutoAssign: &autoAssign,
+				},
+			},
+			isNewAddressPool: true,
+			expectedError:    "invalid IP range",
+		},
+	}
+
+	for _, test := range tests {
+		err := test.addressPool.validateAddressPool(test.isNewAddressPool, addressPoolList)
+		if err == nil {
+			t.Errorf("%s: ValidateAddressPool failed, no error found while expected: \"%s\"", test.desc, test.expectedError)
+		} else {
+			if !strings.Contains(fmt.Sprint(err), test.expectedError) {
+				t.Errorf("%s: ValidateAddressPool failed, expected error: \"%s\" to contain: \"%s\"", test.desc, err, test.expectedError)
+			}
+		}
+	}
+}
