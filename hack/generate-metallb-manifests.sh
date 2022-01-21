4d3b5ea8f4abb215361d5f7036e56524cfee39d3
Federico Paolinelli <fpaoline@redhat.com>
2022-01-21 11:18:38 +0100

    Bump the metallb commit again
    This incorporate the change to make bfd profile name lowercase on all
    the tests.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- hack/generate-metallb-manifests.sh
+++ hack/generate-metallb-manifests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . $(dirname "$0")/common.sh
 
-METALLB_COMMIT_ID="a4b2482c334678ae79d79b5f5b3196ad760b48ac"
+METALLB_COMMIT_ID="4ba7a91e5d8768ac3cbe025058a8da3e82452428"
 METALLB_SC_FILE=$(dirname "$0")/securityContext.yaml
 
 NATIVE_MANIFESTS_FILE="metallb.yaml"
