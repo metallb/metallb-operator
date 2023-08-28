718d7d6d41bf534d2991d543a6d8e14705f72368
Lior Noy <lnoy@redhat.com>
2023-08-28 14:50:04 +0300

    Bump Go to v1.20
    This commit bumps the go version to 1.20 and updates
    also the Dockerfile and workflows.
    
    Signed-off-by: Lior Noy <lnoy@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -2,7 +2,7 @@
 
 . $(dirname "$0")/common.sh
 
-VERSION="v1.45.2"
+VERSION="v1.54.2"
 
 docker run --rm -v $(pwd):/app:z -w /app -e GO111MODULE=on golangci/golangci-lint:${VERSION} \
 	golangci-lint run --verbose --print-resources-usage --timeout=15m0s
