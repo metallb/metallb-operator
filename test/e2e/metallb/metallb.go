82c1c1b77be846f8c4640525f3d8ead0da0315fb
Anvesh J <ajaggapa@redhat.com>
2026-01-07 14:54:27 +0530

    e2e: add logLevel tests for MetalLB core components
    - Add tests to verify logLevel default and debug
    
    Signed-off-by: Anvesh J <ajaggapa@redhat.com>

--- test/e2e/metallb/metallb.go
+++ test/e2e/metallb/metallb.go
@@ -13,6 +13,7 @@ import (
 	"github.com/metallb/metallb-operator/pkg/status"
 	"github.com/metallb/metallb-operator/test/consts"
 	testclient "github.com/metallb/metallb-operator/test/e2e/client"
+	v1 "k8s.io/api/core/v1"
 	schv1 "k8s.io/api/scheduling/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -141,4 +142,111 @@ func DeletePriorityClass(pc *schv1.PriorityClass) {
 		return
 	}
 	Expect(err).ToNot(HaveOccurred())
+}
+
+// WaitForControllerDeploymentReady waits for the MetalLB controller deployment to be ready
+func WaitForControllerDeploymentReady(namespace string, timeout time.Duration) {
+	Eventually(func() error {
+		deploy, err := testclient.Client.Deployments(namespace).Get(context.Background(), consts.MetalLBDeploymentName, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+
+		if deploy.Status.ReadyReplicas != deploy.Status.Replicas {
+			return fmt.Errorf("deployment %s is not ready, expected %d ready replicas got %d", consts.MetalLBDeploymentName, deploy.Status.Replicas, deploy.Status.ReadyReplicas)
+		}
+
+		return nil
+	}, timeout, Interval).ShouldNot(HaveOccurred())
+}
+
+// WaitForSpeakerDaemonSetReady waits for the MetalLB speaker daemonset to be ready
+func WaitForSpeakerDaemonSetReady(namespace string, timeout time.Duration) {
+	Eventually(func() error {
+		daemonset, err := testclient.Client.DaemonSets(namespace).Get(context.Background(), consts.MetalLBDaemonsetName, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+
+		if daemonset.Status.DesiredNumberScheduled == 0 {
+			return fmt.Errorf("daemonset %s has no desired pods scheduled", consts.MetalLBDaemonsetName)
+		}
+		if daemonset.Status.DesiredNumberScheduled != daemonset.Status.NumberReady {
+			return fmt.Errorf("daemonset %s is not ready, expected %d ready pods got %d", consts.MetalLBDaemonsetName, daemonset.Status.DesiredNumberScheduled, daemonset.Status.NumberReady)
+		}
+
+		return nil
+	}, timeout, Interval).ShouldNot(HaveOccurred())
+}
+
+// WaitForProgressingConditionTrue waits for the MetalLB Progressing condition to be True
+func WaitForProgressingConditionTrue(metallb *metallbv1beta1.MetalLB, timeout time.Duration) {
+	Eventually(func() bool {
+		config := &metallbv1beta1.MetalLB{}
+		err := testclient.Client.Get(context.Background(), goclient.ObjectKey{Namespace: metallb.Namespace, Name: metallb.Name}, config)
+		if err != nil {
+			return false
+		}
+		if config.Status.Conditions == nil {
+			return false
+		}
+		for _, condition := range config.Status.Conditions {
+			if condition.Type == status.ConditionProgressing && condition.Status == metav1.ConditionTrue {
+				return true
+			}
+		}
+		return false
+	}, timeout, Interval).Should(BeTrue(), "Progressing condition should be True after update")
+}
+
+// WaitForProgressingFalseAndAvailableTrue waits for the MetalLB Progressing condition to be False and Available condition to be True
+func WaitForProgressingFalseAndAvailableTrue(metallb *metallbv1beta1.MetalLB, timeout time.Duration) {
+	Eventually(func() bool {
+		config := &metallbv1beta1.MetalLB{}
+		err := testclient.Client.Get(context.Background(), goclient.ObjectKey{Namespace: metallb.Namespace, Name: metallb.Name}, config)
+		if err != nil {
+			return false
+		}
+		if config.Status.Conditions == nil {
+			return false
+		}
+		progressingFalse := false
+		availableTrue := false
+		for _, condition := range config.Status.Conditions {
+			if condition.Type == status.ConditionProgressing && condition.Status == metav1.ConditionFalse {
+				progressingFalse = true
+			}
+			if condition.Type == status.ConditionAvailable && condition.Status == metav1.ConditionTrue {
+				availableTrue = true
+			}
+		}
+		return progressingFalse && availableTrue
+	}, timeout, Interval).Should(BeTrue(), "Progressing should be False and Available should be True after update completes")
+}
+
+func DeleteDefaultMetalLB(namespace string, useMetallbResourcesFromFile bool) {
+	metallb, err := Get(namespace, useMetallbResourcesFromFile)
+	Expect(err).ToNot(HaveOccurred(), "Failed to get MetalLB resource")
+	DeleteAndCheck(metallb)
+}
+
+// getMetalLBPods fetches pods for a given MetalLB component (controller or speaker)
+// and validates that the list is not empty
+func getMetalLBPods(namespace string, component string) *v1.PodList {
+	pods, err := testclient.Client.Pods(namespace).List(context.Background(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("component=%s", component),
+	})
+	Expect(err).ToNot(HaveOccurred(), fmt.Sprintf("Failed to list %s pods", component))
+	Expect(len(pods.Items)).Should(BeNumerically(">", 0), fmt.Sprintf("%s Pods List should not be empty", component))
+	return pods
+}
+
+// GetMetalLBControllerPods fetches the controller pods for a given namespace
+func GetMetalLBControllerPods(namespace string) *v1.PodList {
+	return getMetalLBPods(namespace, "controller")
+}
+
+// GetMetalLBSpeakerPods fetches the speaker pods for a given namespace
+func GetMetalLBSpeakerPods(namespace string) *v1.PodList {
+	return getMetalLBPods(namespace, "speaker")
 }
