0d8ac4592884abd01433130a5546df6cee183982
Andreas Karis <ak.karis@gmail.com>
2026-02-23 19:09:42 +0100

    Bump linter to 2.11.3
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -2,7 +2,7 @@
 
 . $(dirname "$0")/common.sh
 
-VERSION="v1.54.2"
+VERSION="v2.11.3"
 
 docker run --rm -v $(pwd):/app:z -w /app -e GO111MODULE=on golangci/golangci-lint:${VERSION} \
 	golangci-lint run --verbose --print-resources-usage --timeout=15m0s
